393bd2c857a4225d93ddd5ef44d90b9fb7457fb4
Intera-PC <Intera-PC@LAPTOP-MPVMO4BN.bs.br.bsch>
2017-10-20 14:28:30 -0200

    thread.wait

---
+++ calculador/src/br/com/alura/threads/Banheiro.java
@@ -0,0 +1,70 @@
+package br.com.alura.threads;
+
+public class Banheiro {
+	
+	private boolean ehSujo = true;
+	
+	private void esperaLaFora(String nome) {
+		System.out.println(nome + ", eca banheiro está sujo");
+		try {
+			this.wait();
+		}catch(InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void fazNumero1() {
+		
+		String nome = Thread.currentThread().getName();
+		
+		System.out.println(nome + " batendo na porta");
+		
+		synchronized(this) {
+			System.out.println(nome + "Entrando no banheiro");
+			
+			if(this.ehSujo) {
+				esperaLaFora(nome);
+			}
+			
+			System.out.println(nome + "Fazendo coisa rápida");
+			
+			try {
+				Thread.sleep(8000);
+			}catch(InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+			System.out.println(nome + "Dando discarga");
+			System.out.println(nome + "Lavando a mão");
+			System.out.println(nome + "Saindo do banheiro");
+		}
+		
+		
+	}
+	
+	public void fazNumero2() {
+		String nome = Thread.currentThread().getName();
+		
+		System.out.println(nome + " batendo na porta");
+		
+		synchronized(this) {
+			System.out.println(nome + "Entrando no banheiro");
+			
+			if(this.ehSujo) {
+				esperaLaFora(nome);
+			}
+			
+			System.out.println(nome + "Fazendo coisa demorada");
+			
+			try {
+				Thread.sleep(15000);
+			}catch(InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+			System.out.println(nome + "Dando a discarga");
+			System.out.println(nome + "Lavando a mão");
+			System.out.println(nome + "Saindo do banheiro");
+		}
+	}
+}
---
+++ calculador/src/br/com/alura/threads/BanheiroTest.java
@@ -0,0 +1,19 @@
+package br.com.alura.threads;
+
+public class BanheiroTest {
+	public static void main(String[] args) {
+		Banheiro banheiro = new Banheiro();
+		
+		Thread convidado1 = new Thread(new TarefaNumero1(banheiro), "João");
+		Thread convidado2 = new Thread(new TarefaNumero2(banheiro), "Pedro");
+		Thread convidado3 = new Thread(new TarefaNumero1(banheiro),"Ana");
+		Thread convidado4 = new Thread(new TarefaNumero2(banheiro),"Maria");
+		
+		convidado1.start();
+		convidado2.start();
+		convidado3.start();
+		convidado4.start();
+		
+		
+	}
+}
